8991f818f975e4fe96ad92a7baca137cdabf3033
Choi-Jinwook <wlsdnr21@naver.com>
2022-11-21 19:45:24 +0900

    devide_folder

---
+++ baekjoon/조건문/조건문_9498_시험성적.js
@@ -0,0 +1,28 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", (line) => {
+  input.push(line);
+  rl.close();
+});
+
+rl.on("close", () => {
+  if (input[0] <= 100 && input[0] >= 90) {
+    console.log("A");
+  } else if (input[0] >= 80 && input[0] < 90) {
+    console.log("B");
+  } else if (input[0] >= 70 && input[0] < 80) {
+    console.log("C");
+  } else if (input[0] >= 60 && input[0] < 70) {
+    console.log("D");
+  } else {
+    console.log("F");
+  }
+  process.exit();
+});
