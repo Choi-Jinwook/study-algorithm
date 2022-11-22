15c1f42ac2d79e5f3e852f451bcf88fcf0da6875
Choi-Jinwook <wlsdnr21@naver.com>
2022-11-22 22:28:22 +0900

    15552

---
+++ baekjoon/반복문/반복문_15552_빠른A+B.js
@@ -0,0 +1,29 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let t;
+let ans = "";
+
+rl.on("line", (line) => {
+  if (line.includes(" ")) {
+    input.push(line.split(" ").map((el) => parseInt(el)));
+  } else {
+    t = parseInt(line);
+  }
+  if (input.length === t) {
+    rl.close();
+  }
+});
+
+rl.on("close", () => {
+  for (let i = 0; i < t; i++) {
+    ans += `${input[i][0] + input[i][1]}\n`;
+  }
+  console.log(ans);
+  process.exit();
+});
