c488854700d23679a11d9b4b7ad5170dec0bd5c9
Choi-Jinwook <wlsdnr21@naver.com>
2022-11-24 16:35:45 +0900

    10818

---
+++ baekjoon/1차원배열/1차원배열_10818_최소최대.js
@@ -0,0 +1,29 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let n = 0;
+let input = [];
+
+rl.on("line", (line) => {
+  if (n === 0) {
+    n = parseInt(line);
+  } else {
+    input = line.split(" ").map((el) => parseInt(el));
+  }
+}).on("close", () => {
+  let min = input[0];
+  let max = input[0];
+  for (let i = 0; i < n; i++) {
+    if (min > input[i]) {
+      min = input[i];
+    }
+    if (max < input[i]) {
+      max = input[i];
+    }
+  }
+  console.log(min + " " + max);
+});
