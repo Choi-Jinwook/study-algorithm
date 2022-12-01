91f4230094c2f012ee892f379496ea95e11bb5a9
Choi-Jinwook <wlsdnr21@naver.com>
2022-12-01 12:35:45 +0900

    10870

--- baekjoon/재귀/재귀_10870_피보나치수5.js
+++ baekjoon/재귀/재귀_10870_피보나치수5.js
@@ -6,3 +6,23 @@ const rl = readline.createInterface({
 });
 
 let n = 0;
+let result = 0;
+let i = 2;
+let input = [0, 1];
+
+const fibo = (num) => {
+  if (num === 0) return result;
+  input.push(parseInt(input[i - 1] + input[i - 2]));
+  i++;
+  result = input[i - 1];
+  if (i > n) return result;
+  fibo(i);
+};
+
+rl.on("line", (line) => {
+  n = parseInt(line);
+}).on("close", () => {
+  fibo(n);
+  console.log(result);
+  process.exit();
+});
