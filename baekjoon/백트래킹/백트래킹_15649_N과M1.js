160adef41f88baa07555483c98c84f441ef9ff71
Choi-Jinwook <wlsdnr21@naver.com>
2022-12-06 15:54:12 +0900

    15649

---
+++ baekjoon/백트래킹/백트래킹_15649_N과M1.js
@@ -0,0 +1,40 @@
+let result = [];
+let list = [];
+let check = new Array(9).fill(false);
+const re = (cnt, n, m) => {
+  if (cnt === m) {
+    result.push(list.join(" "));
+    return 1;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (!check[i]) {
+      check[i] = true;
+      list[cnt] = i;
+      re(cnt + 1, n, m);
+      console.log(check);
+      check[i] = false;
+    }
+  }
+  return 1;
+};
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", (line) => {
+  input = line.split(" ").map((el) => parseInt(el));
+}).on("close", () => {
+  let n = input[0];
+  let m = input[1];
+  re(0, n, m);
+  console.log(result.join("\n"));
+
+  process.exit();
+});
